839a591c20386bd8a8860762d0f7b423dbd9f4f1
gelthemu <gelthemu@gmail.com>
2026-02-03 12:45:35 +0300

    auto

--- src/app/intellirdb/components/docs/doc-view.tsx
+++ src/app/intellirdb/components/docs/doc-view.tsx
@@ -36,7 +36,7 @@ const DocView: React.FC<DocViewProps> = ({ doc }) => {
 
   const renderMarkdown = (content: string) => {
     return (
-      <div className="prose prose-lg max-w-none select-text">
+      <div className="prose prose-lg max-w-xl select-text">
         <Markdown rehypePlugins={[rehypeRaw]} components={mdFeed}>
           {content}
         </Markdown>
